6c7b82010a23e09131fbb857f9468c72e3fb29a4
inwebo <github@inwebo.net>
2020-05-25 21:54:43 +0200

    Delete useless

--- src/Cell/Cell.js
+++ src/Cell/Cell.js
@@ -2,13 +2,7 @@ import {Vector2D} from "@inwebo/vector";
 import {Sprite} from "@inwebo/sprite.js";
 
 export default class Cell {
-    /**
-     * @param {number} index
-     * @param {Sprite|null} sprite
-     */
-    constructor(index, sprite = null) {
-        this._origin      = index;
-        this._sprite      = sprite;
-        this._isBuildalbe = true;
+
+    constructor() {
     }
 }
