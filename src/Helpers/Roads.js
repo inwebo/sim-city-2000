ab50b5e7b13505552f863a1d46db404f8aa8c88b
inwebo <github@inwebo.net>
2020-06-01 08:01:02 +0200

    Add roads builder

---
+++ src/Helpers/Roads.js
@@ -0,0 +1,14 @@
+import {Vector2D} from "@inwebo/vector";
+
+export default class Roads {
+    /**
+     * @param {Vector2D} chunkDimension
+     */
+    constructor(chunkDimension) {
+        this._chunkDimension = chunkDimension;
+    }
+
+    build() {
+
+    }
+}
