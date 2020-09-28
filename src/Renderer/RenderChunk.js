675287de72edf238178fc029bc0be0883c99d6d9
inwebo <github@inwebo.net>
2020-09-28 21:32:16 +0200

    Refacto

--- src/Renderer/RenderChunk.js
+++ src/Renderer/RenderChunk.js
@@ -1,6 +1,3 @@
-import {Vector2D} from "@inwebo/vector";
 import AbstractRender from "./AbstractRender";
 
-export default class RenderChunk extends AbstractRender {
-
-}
+export default class RenderChunk extends AbstractRender {}
