20a241ee6986dd84e1e6d3b77e20b3bb928949ed
julien <julien@ddf.agency>
2020-09-29 14:02:57 +0200

    Update roads generate()

--- src/World/Roads.js
+++ src/World/Roads.js
@@ -1,8 +1,5 @@
 import {Chunk} from "../index";
-import Cell from "../Cell/Cell";
-import {Vector2D} from "@inwebo/vector";
 import rand from "../Helpers/Rand";
-import ArrayHelper from "../Helpers/ArrayHelper";
 
 export default class Roads extends Chunk {
 
@@ -16,67 +13,41 @@ export default class Roads extends Chunk {
         let randX = rand(0, this.getDimensions().getX());
         let randY = rand(0, this.getDimensions().getY());
 
-        // if(randX % 2 !== 0 && randY % 2 === 0 ) {
-        //     randX += 1;
-        //     if(randX >= this.getDimensions().getX()) {
-        //         randX = this.getDimensions().getX() - 1;
-        //     }
-        // }
-        //
-        // if(randY % 2 !== 0 && randX % 2 === 0 ) {
-        //     randY += 1;
-        //     if(randY > this.getDimensions().getY()) {
-        //         randY = this.getDimensions().getY() - 1;
-        //     }
-        // }
+        let x = 0, y = 0;
 
-        let loop = 0;
-        let x, y = 0;
+        for (let loop = 0; loop < 4; loop++) {
 
 
-        // 9, 2
-        // 6, 10
-        // 10, 10
+            if (this.getCells().getCell(x, y) === false) {
+                break;
+            }
 
-        while (this.getCells().hasCell(loop,0) !== false) {
 
-            loop++;
+            if(loop === 0) {
+                this.getCells().getCell(x, y).setHasRoad(true);
+            }
+
+            if (loop % 2 !== 0 && loop !== 0) {
+                x++;
+            }
+            // this.getCells().getCell(x, y).setHasRoad(true);
+            console.log(loop, x, y);
+
+            y++;
+            if(loop > 0) {
+                this.getCells().getCell(x, y).setHasRoad(true);
+            }
         }
 
-        // console.log(this.getCells().getCell(0,0));
-        this.getCells().getCell(0,0).setHasRoad(true);
+        // this.getCells().getCell(0,0).setHasRoad(true);
 
-        this.getCells().getCell(1,1).setHasRoad(true);
-        this.getCells().getCell(1,2).setHasRoad(true);
-
-        this.getCells().getCell(2,3).setHasRoad(true);
-        this.getCells().getCell(2,4).setHasRoad(true);
-
-        this.getCells().getCell(3,5).setHasRoad(true);
-        this.getCells().getCell(3,5).setHasRoad(true);
-
-
+        // this.getCells().getCell(1,1).setHasRoad(true);
         // this.getCells().getCell(1,2).setHasRoad(true);
+        //
         // this.getCells().getCell(2,3).setHasRoad(true);
         // this.getCells().getCell(2,4).setHasRoad(true);
+        //
         // this.getCells().getCell(3,5).setHasRoad(true);
         // this.getCells().getCell(3,6).setHasRoad(true);
-
-
-
-
-        // for (let i = 0; i < 10; i++) {
-        //     try {
-        //         // const hasRoad = this.getCell(randX + i ,randY + i);
-        //         if(hasRoad) {
-        //             // this.getCell(randX + i ,randY + i).setHasRoad(true);
-        //         }
-        //
-        //     } catch (e) {
-        //         console.error(e);
-        //         // cell doesnt exists
-        //         break;
-        //     }
-        // }
     }
 }
