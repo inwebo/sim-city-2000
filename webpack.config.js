1e17948c0d2d7616ea7508b6aa0e3ea56748a345
inwebo <github@inwebo.net>
2020-10-02 08:20:57 +0200

    Update conf

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 
     output: {
         filename: (pathData) => {
-            return (pathData.chunk.name === 'mario' || pathData.chunk.name === 'demo') ? 'docs/assets/js/[name].min.js' : 'dist/[name].min.js';
+            return (pathData.chunk.name === 'demo') ? 'docs/assets/js/[name].min.js' : 'dist/[name].min.js';
         },
         path: path.resolve(__dirname, '.'),
     },
