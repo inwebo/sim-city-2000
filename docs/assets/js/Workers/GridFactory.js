39505f185fe0dd0aad4905b7135ac9afa3d82ccf
inwebo <github@inwebo.net>
2020-12-28 17:48:24 +0100

    Useless

--- docs/assets/js/Workers/GridFactory.js
+++
@@ -1,8 +0,0 @@
-import {Vector2D} from "@inwebo/vector";
-import {Grid} from "@inwebo/grid.js";
-
-onmessage((e)=>{
-    const grid = new Grid(new Vector2D(25, 25));
-    console.log(grid);
-    postMessage(grid);
-});
