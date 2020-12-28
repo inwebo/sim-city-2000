96f73bf291ff824bcbdf9d29cefb0e594867db52
inwebo <github@inwebo.net>
2020-12-28 17:48:36 +0100

    Add * getCellByType()

--- src/Grid/Grid.js
+++ src/Grid/Grid.js
@@ -1,7 +1,11 @@
 import {Grid as BaseGrid} from "@inwebo/grid.js";
 
 export default class Grid extends BaseGrid {
-    getBuildings() {
-
+    * getCellByType() {
+        for(const cell of this.getGenerator()) {
+            if(cell.getSize() !== null) {
+                yield cell;
+            }
+        }
     }
 }
