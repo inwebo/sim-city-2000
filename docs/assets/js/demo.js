a455f1e67f2e5257295f2d9c4042d25e21161afa
inwebo <github@inwebo.net>
2020-12-28 17:49:19 +0100

    Bump demo

--- docs/assets/js/demo.js
+++ docs/assets/js/demo.js
@@ -4,27 +4,57 @@ import {AssetsLoader} from '@inwebo/assetsloader.js'
 import {Vector2D} from '@inwebo/vector';
 import GridRenderer from "../../../src/Renderer/GridRenderer";
 import Chunk from "../../../src/Chunk/Chunk";
-import {Grid} from "@inwebo/grid.js";
 
+import Grid from "../../../src/Grid/Grid";
+import Cell from "../../../src/Grid/Cell";
+import populateGrid from "../../../src/World/PopulateBuildings"
+
+import conf from "../../../src/config.json"
+import {Cartesian} from "@inwebo/grid.js";
 
 window.addEventListener("DOMContentLoaded", (event) => {
     const worldCanvas  = document.getElementById('world');
-    const tilesSheet   = AssetsLoader.image('assets/img/tile-cartesian-default.png');
-    const tilesJSONMap = AssetsLoader.json('assets/img/tile-cartesian-default.json');
 
-    Promise.all([tilesSheet, tilesJSONMap])
-        .then(([tilesSheet, tilesJSONMap]) => {
+    const tilesSheet         = AssetsLoader.image('assets/img/tiles.png');
+    const tilesJSONMap       = AssetsLoader.json("assets/img/tiles.json");
 
+    const infraTilesSheet   = AssetsLoader.image('assets/img/infrastructures.png');
+    const infraTilesJSONMap = AssetsLoader.json('assets/img/infrastructures.json');
+
+    Promise.all([tilesSheet, tilesJSONMap, infraTilesSheet, infraTilesJSONMap])
+        .then(([tilesSheet, tilesJSONMap, infraTilesSheet, infraTilesJSONMap]) => {
             // region
+
             const tilesOffScreenRender = new RenderOffScreen(worldCanvas, tilesSheet);
             const tilesSpriteMap       = new SpriteMap(tilesJSONMap, tilesOffScreenRender.getCtx());
 
             const size                 = new Vector2D(10,10);
-            const cells                = new Grid(size);
-            const chunk                = new Chunk(size, cells);
 
-            const gridRenderer = new GridRenderer(worldCanvas);
-            gridRenderer.draw(chunk, tilesSpriteMap.get('tile-1'));
+            const grid = new Grid(size, null, ([x, y]) => {
+                return new Cell(new Vector2D(x, y));
+            });
+
+            while (grid.assertCells((cell) => {
+                return cell.getType() === null;
+            }) !== false) {
+                const start = grid.assertCells((cell) => {
+                    return cell.getType() === null;
+                });
+
+                populateGrid(grid, start);
+            }
+
+            console.log(conf)
+            console.log(grid.getRows())
+
+            const gridRenderer = new GridRenderer(worldCanvas, new Cartesian());
+            gridRenderer.draw(grid, tilesSpriteMap.get('tiles-1'));
+
+            const buildable = grid.getCellByType();
+            for (const b of buildable) {
+                console.log(b);
+            }
+
             // endregion
 
         })
