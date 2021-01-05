fa2b45db054a3c4337998536e5921db4f0499225
inwebo <github@inwebo.net>
2021-01-05 18:49:26 +0100

    Refactoring

--- src/Renderer/GridRenderer.js
+++ src/Renderer/GridRenderer.js
@@ -22,20 +22,8 @@ export default class GridRenderer extends RendererAbstract {
         let y = (cell.getIndex().getX() + cell.getIndex().getY()) * this._cellToBitmapDimensions.getY()/2 + this._viewOrigin.getY();
 
         if(sprite !== null) {
-            if(cell.getSize() === 1) {
-                y -= (sprite.height - (this._cellToBitmapDimensions.getY())) - 1;
-                x -= (cell.getSize() - 1) *  this._cellToBitmapDimensions.getX() / 2;
-            }
-
-            if(cell.getSize() === 2) {
-                y -= (sprite.height - (this._cellToBitmapDimensions.getY())) - 1;
-                x -= (cell.getSize() - 1) *  this._cellToBitmapDimensions.getX() / 2;
-            }
-
-            if(cell.getSize() === 3) {
-                y -= (sprite.height - (this._cellToBitmapDimensions.getY()) * 3) - 1;
-                x -= (cell.getSize() - 1) *  this._cellToBitmapDimensions.getX() / 2;
-            }
+            y -= (sprite.height - (this._cellToBitmapDimensions.getY()) * cell.getSize()) - 1;
+            x -= (cell.getSize() - 1) *  this._cellToBitmapDimensions.getX() / 2;
         }
 
         return new Vector2D(x, y);
